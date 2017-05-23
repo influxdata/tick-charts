0748a3412919ecbf431a41efcbfb3d171b581984
Michael Lieberman <mlieberman85@gmail.com>
2017-05-23 16:49:56 -0400

    Fix typo in create.sh

--- create.sh
+++ create.sh
@@ -5,4 +5,4 @@ helm install --name polling --namespace tick ./telegraf-s/
 helm install --name hosts --namespace tick ./telegraf-ds/
 helm install --name alerts --namespace tick ./kapacitor/
 helm install --name dash --namespace tick ./chronograf/
-kkubectl get svc -w --namespace tick -l app=dash-chronograf
+kubectl get svc -w --namespace tick -l app=dash-chronograf
