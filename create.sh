1fcfcbdd7a9de33304f5a1ba334a2bd6034d338c
Tomas Klapka <tomas.klapka@bonitoo.io>
2019-02-04 10:50:50 +0100

    Adding aws-eks deployment script

--- create.sh
+++ create.sh
@@ -1,8 +1,56 @@
 #!/bin/bash
+source ~/.bashrc
+function main
+{
+	initScript "$@"
 
-helm install --name data --namespace tick ./influxdb/
-helm install --name polling --namespace tick ./telegraf-s/
-helm install --name hosts --namespace tick ./telegraf-ds/
-helm install --name alerts --namespace tick ./kapacitor/
-helm install --name dash --namespace tick ./chronograf/
-kubectl get svc -w --namespace tick -l app=dash-chronograf
+	echo "Services:" ${SERVICES[@]:-all}
+	echo "Action:" $ACTION
+
+	if [[ $PROVIDER == 'aws-eks' ]]; then
+		echo "Provider:" $PROVIDER
+		#chart_execution $ACTION $PROVIDER $SERVICE
+		scripts/aws-eks.sh $ACTION ${SERVICES[@]}
+
+	elif [[ $PROVIDER == '' ]]; then
+		echo "Provider name is empty"
+	else
+		echo "Provider name is not valid !!!"
+	fi
+}
+
+function usage
+{
+	cat <<EOF
+
+    Usage:
+        -p provider: Valid option is aws-eks
+        -a action: Valid options are create and destroy
+        -s services: The name of the component. Valid options are influxdb, kapacitor, telegraf-s, telegraf-ds, chronograf or all
+    Examples:
+        ./create.sh -s influxdb -a create -c aws-eks
+        ./create.sh -s influxdb -a destroy -c aws-eks
+
+        ./create.sh -s all -a create -c aws-eks
+        ./create.sh -s all -a delete -c aws-eks
+EOF
+}
+
+function initScript
+{
+	PROVIDER=""
+	ACTION="create"
+	SERVICES=""
+	while getopts h:a:c:s: opt
+		do
+			case "$opt" in
+				h) usage "";exit 1;;
+				c) PROVIDER=$OPTARG;;
+				a) ACTION=$OPTARG;;
+				s) SERVICES+=($OPTARG);;
+				\?) usage "";exit 1;;
+			esac
+		done
+
+}
+main "$@"
